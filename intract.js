518765e8b61ea32a50afb990f39669f0c6f7dd25
DHANUSHKODIAJJ <144648673+DHANUSHKODIAJJ@users.noreply.github.com>
2023-12-09 12:19:14 +0530

    added loging page

---
+++ intract.js
@@ -0,0 +1,4 @@
+const a=prompt("please enter a number:");
+const b=prompt("please enter the number:");
+const c=a+b;
+console.log(c)
---
+++ script.js
@@ -0,0 +1,8 @@
+var signin=document.querySelector(".logo2")
+var box2=document.querySelector(".signup-box")
+var box1=document.querySelector(".box")
+
+signin.addEventListener("click",function(){
+    box2.style.display="flex"
+    box1.style.display="none"
+})
